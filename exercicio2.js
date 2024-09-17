d328a581022a94fa8375c017ec18c8e74f4325f3
wfzG <166777068+wfzG@users.noreply.github.com>
2024-09-16 21:32:41 -0300

    Add files via upload
    VAR - Exercicio 2

---
+++ exercicio2.js
@@ -0,0 +1,10 @@
+let idade = 21
+
+console.log(idade)
+
+idade = idade + idade
+
+console.log(idade)
+
+//criei uma variavel mutavel (let), e apos eu imprimi.
+//depois reatribui o valor da variavel "idade + idade" e imprimi o dobro da primeira variavel
